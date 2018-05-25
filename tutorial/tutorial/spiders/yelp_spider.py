8d49af5befcbdf2a9094dd3bad5ad7afbda85b45
Lu He <luhe@dhcp-v005-029.mobile.uci.edu>
2018-05-24 20:33:59 -0700

    finish crawling reviews

--- tutorial/tutorial/spiders/yelp_spider.py
+++ tutorial/tutorial/spiders/yelp_spider.py
@@ -12,34 +12,26 @@ class QuotesSpider(scrapy.Spider):
 
     def __init__(self):
         path = os.getcwd() + '/book_keeping/'
-        os.mkdir(path, 0755)
+        #os.mkdir(path, 0755)
         print "Path is created"
         self.link_file = path + 'links.txt'
 
 
     def start_requests(self):
         #read in a url-list file 
-        urls = [
-            'https://www.yelp.com/biz/newport-center-urgent-care-newport-beach'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        root_file = os.getcwd() + '/book_keeping/root_links.txt'
+        fronts = [line.rstrip('\n') for line in open(root_file)]
+        for url in fronts:
+            yield scrapy.Request(url=url, callback=self.get_all_pages)
+        pages = [line.rstrip('\n') for line in open(self.link_file)]
+        for page in pages:
+            yield scrapy.Request(url=page, callback=self.parse)
+
+    
 
     def parse(self, response):
         reviews = response.css('div#wrap.lang-en>div.biz-country-us>div.main-content-wrap>div#super-container>div.container>div.clearfix>div.column>div>div.feed>div.review-list>ul>li')
-        pages = response.css('div#wrap.lang-en>div.biz-country-us>div.main-content-wrap>div#super-container>div.container>div.clearfix>div.column>div>div.feed>div.review-pager>div.pagination-block>div.arrange>div.pagination-links>div.arrange')
-        #print next_page
-        with open(self.link_file, 'a') as fout:
-            for page in pages:
-                next_page = page.css('div.arrange_unit>a::attr(href)').extract()
-                for ele in next_page[0:len(next_page)-1]:
-                    fout.write(ele.encode('utf-8'))
-                    fout.write('\n')
-            
-        #uls = response.xpath('//ul.ylist')
-        with open('content_2.txt', 'w') as fout:
-            fout.write(('   ').join(['user_ID', 'content', 'upvotes', 'star']))
-            fout.write('\n')
+        with open('content.txt', 'a') as fout:
             for review in reviews[1: len(reviews)]:
                 content = review.css('div.review>div.review-wrapper>div.review-content>p::text').extract()
                 content = '.'.join(content).encode('utf-8')
@@ -49,3 +41,16 @@ class QuotesSpider(scrapy.Spider):
                 star = review.css('div.review>div.review-wrapper>div.review-content>div.biz-rating>div>div::attr(title)').extract()[0].encode('utf-8')
                 fout.write('\t'.join([user, content, upvotes, star]))
                 fout.write('\n')
+
+    def get_all_pages(self, response):
+        pages = response.css('div#wrap.lang-en>div.biz-country-us>div.main-content-wrap>div#super-container>div.container>div.clearfix>div.column>div>div.feed>div.review-pager>div.pagination-block>div.arrange>div.pagination-links>div.arrange')
+            #print next_page
+        with open(self.link_file, 'a') as fout:
+            for page in pages:
+                next_page = page.css('div.arrange_unit>a::attr(href)').extract()
+                for ele in next_page[0:len(next_page)-1]:
+                    fout.write(ele.encode('utf-8'))
+                    fout.write('\n')
+
+
+    
